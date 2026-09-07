4d94de54c90e6a4a1ace5ebaed7f047782959a1b
Rock Clapps <rockclapps@gmail.com>
2026-09-07 15:43:02 -0400

    adding functions

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,30 +1,18 @@
-// var keepAboveMaximized = new Array();
-// 
-// function manageKeepAbove(client, h, v) {
-//   client.keepAbove = false;
-//   if (h && v) {
-//     // maximized
-//     if (client.keepAbove) {
-//       keepAboveMaximized[keepAboveMaximized.length] = client;
-//       client.keepAbove = false;
-//     }
-//   } else {
-//     // no longer maximized
-//     var found = keepAboveMaximized.indexOf(client);
-//     if (found != -1) {
-//       client.keepAbove = true;
-//       keepAboveMaximized.splice(found, 1);
-//     }
-//   }
-// }
-
 var padding = 8;
 var dockheight = 32;
 var desktopTracker = new Map();
 
-function tileWindow(window) {
-  var currentDesktop = workspace.currentDesktop;
-  if (desktopTracker.get(currentDesktop).length == 1) {
+function windowIsTilable(window) {
+  if (!window.normalWindow || 
+    window.specialWindow || 
+    window.fullscreen ) { 
+    return false; 
+  }
+  return true;
+}
+
+function placePanelInSlot(slot, window){
+  if (slot == 1) {
     window.frameGeometry = { 
       x: 0 + padding, 
       y: dockheight + padding, 
@@ -33,7 +21,7 @@ function tileWindow(window) {
     }
     window.keepBelow = true;
   }
-  if (desktopTracker.get(currentDesktop).length == 2) {
+  if (slot == 2) {
     window.frameGeometry = { 
       x: (workspace.activeScreen.geometry.width / 2) + padding, 
       y: dockheight + padding, 
@@ -42,40 +30,52 @@ function tileWindow(window) {
     }
     window.keepBelow = true;
   }
-
-  adjustFocus(window);
 }
 
-function unTileWindow(window) {
-  adjustFocus(window);
+function refocusOnIndex(index){
+  var currentDesktop = workspace.currentDesktop;
+  if (!desktopTracker.has(currentDesktop) || 
+    desktopTracker.get(currentDesktop).length == 0) {
+    return;
+  }
+
+  var windowList = desktopTracker.get(currentDesktop);
+  for (var i = 0; i < windowList.length; i++) {
+    if (i == index){
+      windowList[i].minimized = false;
+      placePanelInSlot(1, windowList[i]);
+    } else if (i == index + 1){
+      windowList[i].minimized = false;
+      placePanelInSlot(2, windowList[i]);
+    } else {
+      windowList[i].minimized = true;
+    }
+  }
 }
 
-function adjustFocus(window) {
+function tileWindow(window) {
+  var currentDesktop = workspace.currentDesktop;
+  if (!desktopTracker.has(currentDesktop)){
+    desktopTracker.set(currentDesktop, new Array());
+  }
+
+  var windowList = desktopTracker.get(currentDesktop);
+  windowList.push(window);
+
+  if ( windowList.length <= 2 ) {
+    refocusOnIndex(0);
+  } else {
+    refocusOnIndex(windowList.length - 2);
+  }
 }
 
 function addWindow(window) {
-  if (!window.normalWindow || window.fullscreen) { return; }
-
-  var currentDesktop = workspace.currentDesktop;
-  if (!desktopTracker.has(currentDesktop)) {
-    desktopTracker.set(currentDesktop, new Array());
+  if ( windowIsTilable(window) ) {
+    tileWindow(window);
   }
-  var winList = desktopTracker.get(currentDesktop);
-  winList.push(window);
-
-  tileWindow(window);
 }
 
 function removeWindow(window) {
-  if (!window.normalWindow || window.fullscreen) { return; }
-
-  var currentDesktop = workspace.currentDesktop;
-  var winList = desktopTracker.get(currentDesktop);
-
-  var winIndex = winList.indexOf(window);
-  winList.splice(winIndex, 1);
-
-  unTileWindow(window);
 }
 
 workspace.windowAdded.connect(addWindow);
